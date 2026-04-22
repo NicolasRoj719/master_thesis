0ff85b89830baaa6fcee8678d9cbc4de227aad66
Nicolas Rojas <nicolasrs.rojas@gmail.com>
2026-04-22 12:46:15 +0200

    On branch sparsity  Changes to be committed: 	modified:   generator.hpp 	modified:   tests/generator.cpp
    Description:
      The method build_problem was implemented withing the new n_m_n_E_nnz
      generator class. Error handling for invalid arguments was also
      implemented inside generator.hpp file. Finally, generator.cpp was
      rewritten to achieve an interactive testing of the generator.

--- generator.hpp
+++ generator.hpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
 #ifndef GENERATOR_HPP
 #define GENERATOR_HPP
@@ -14,7 +15,17 @@ class Generator{
     chain_length(chain_length_),
     jac_chain_info(chain_length_, std::vector<std::size_t>(inner_size)),
     dim_lb(dim_lb_), dim_ub(dim_ub_), n_E_lb(n_E_lb_), n_E_ub(n_E_ub_),
-    is_deterministic(is_deterministic_), seed(seed_){}
+    is_deterministic(is_deterministic_), seed(seed_){
+      if(chain_length == 0){
+        throw std::invalid_argument("Chain length must be greater than 0.");
+      }
+      if(dim_ub < dim_lb){
+        throw std::invalid_argument("Dimension lower bound must be smaller or equal to the upper bound.");
+      }
+      if(n_E_ub < n_E_lb){
+        throw std::invalid_argument("The lower bound for the number of edges must be smaller or equal to the upper bound.");
+      }
+    }
 
   virtual void print_format() = 0;
 
@@ -35,7 +46,7 @@ class Generator{
 
   std::size_t get_problem_size(){return jac_chain_info.size();}
 
-  const std::vector<std::vector<std::size_t>> get_problem(){
+  const std::vector<std::vector<std::size_t>>& get_problem(){
     return jac_chain_info;
   }
 
@@ -61,8 +72,6 @@ class Generator{
 // m_n_n_E_Generator populates jac_chain_info data structure
 // following the following format
 // n_0 m_0 n_E_0
-// ...
-// n_chain_length-1 m_chain_length-1 n_E_chain_length-1
 // with n,m and n_E taken randomely form a uniform distribution.
 class n_m_n_E_Generator: public Generator{
  public:
@@ -110,6 +119,104 @@ class n_m_n_E_Generator: public Generator{
   void print_format() override{
     std::cout<<"Jacobian information: F'_i: [ n_i m_i n_E_i ]\n";
   }
+};
+
+// m_n_n_E_nnz_Generator populates jac_chain_info data structure
+// following the following format. Inner size equals 4.
+// n_0 m_0 n_E_0 nnz_0
+// with n,m and n_E taken randomely form a uniform distribution.
+// Additionally the m_n_n_E_nnz_Generator generates non zero
+// coordinate file following the format
+// #matrix 1
+// r_0 c_0
+// ...
+// r_last c_last
+// #matrix 2
+// r_0 c_0
+// ...
+//
+class n_m_n_E_nnz_Generator: public Generator{
+ public:
+  n_m_n_E_nnz_Generator(std::size_t chain_length_, std::size_t dim_lb_,
+    std::size_t dim_ub_, std::size_t n_E_lb_, std::size_t n_E_ub_,
+    double den_lb, double den_ub, bool is_deterministic_ = 0, std::size_t seed_ = 0):
+    Generator(chain_length_, 4, dim_lb_, dim_ub_, n_E_lb_, n_E_ub_,
+        is_deterministic_, seed_), density_lb(den_lb), density_ub(den_ub){
+      if(1 < den_ub || 1 < den_lb || den_lb < 0 || density_ub < 0){
+        throw std::invalid_argument("Density must be an element of (0,1).");
+      }
+      if(den_ub < den_lb){
+        throw std::invalid_argument("Density lower bound must be smaller or equal to the upper bound.");
+      }
+    }
+
+  void build_problem(){
+    std::default_random_engine g;
+    
+    if(!is_deterministic){
+      std::random_device r;
+      g.seed(r());
+    }
+
+    else{
+      g.seed(seed);
+    }
+
+
+    std::uniform_int_distribution<std::size_t> d_dim_in_out(dim_lb, dim_ub);
+    std::uniform_int_distribution<std::size_t> d_n_E(n_E_lb, n_E_ub);
+    std::uniform_int_distribution<std::size_t> d_nnz;
+
+    std::size_t jac_index=0;
+    const std::size_t n=0, m=1, n_E=2, nnz=3;
+
+    jac_chain_info[jac_index][m] = d_dim_in_out(g);
+    jac_chain_info[jac_index][n] = d_dim_in_out(g);
+    jac_chain_info[jac_index][n_E] = d_n_E(g);
+    
+    //Minimum density of a single non zero element per row.
+    std::size_t nnz_lb, nnz_ub, nm;
+    nm = jac_chain_info[jac_index][n] * jac_chain_info[jac_index][m];
+
+    if(jac_chain_info[jac_index][m] < static_cast<std::size_t>(1/density_lb)){
+      nnz_lb = jac_chain_info[jac_index][n];
+    }
+    else{
+      nnz_lb = static_cast<std::size_t>(density_lb * nm);
+    }
+    nnz_ub = static_cast<std::size_t>(density_ub * nm);
+
+    std::uniform_int_distribution<std::size_t>::param_type p(nnz_lb, nnz_lb);
+    jac_chain_info[jac_index][nnz] = d_nnz(g, p);
+    jac_index++;
+
+    while(jac_index < chain_length){
+      jac_chain_info[jac_index][n] = jac_chain_info[jac_index-1][m];
+      jac_chain_info[jac_index][m] = d_dim_in_out(g);
+      jac_chain_info[jac_index][n_E] = d_n_E(g);
+
+      nm = jac_chain_info[jac_index][n] * jac_chain_info[jac_index][m];
+      if(jac_chain_info[jac_index][m] < static_cast<std::size_t>(1/density_lb)){
+        nnz_lb = jac_chain_info[jac_index][n];
+      }
+      else{
+        nnz_lb = static_cast<std::size_t>(density_lb * nm);
+      }
+      nnz_ub = static_cast<std::size_t>(density_ub * nm);
+
+      std::uniform_int_distribution<std::size_t>::param_type p(nnz_lb, nnz_ub);
+      jac_chain_info[jac_index][nnz] = d_nnz(g, p);
+      jac_index++;
+    }
+  }
+
+  void print_format() override{
+    std::cout<<"Jacobian information: F'_i: [ n_i m_i n_E_i nnz ]\n";
+  }
+
+ private:
+  double density_lb;
+  double density_ub;
 };
 
 #endif
--- tests/generator.cpp
+++ tests/generator.cpp
@@ -1,65 +1,99 @@
 #include <cassert>
 #include <cstdint>
+#include <iostream>
+#include <string>
+#include <stdexcept>
 #include "./../generator.hpp"
 
-int main(int argc, char *v[]){
-  assert(argc == 7 || argc == 8);
-  std::size_t chain_len = static_cast<std::size_t>(std::stoi(v[1]));
-  std::size_t dim_lb= static_cast<std::size_t>(std::stoi(v[2]));
-  std::size_t dim_ub= static_cast<std::size_t>(std::stoi(v[3]));
-  std::size_t n_E_lb= static_cast<std::size_t>(std::stoi(v[4]));
-  std::size_t n_E_ub= static_cast<std::size_t>(std::stoi(v[5]));
-  bool is_deterministic = static_cast<std::size_t>(std::stoi(v[6]));
-  std::size_t seed=0;
-  if(is_deterministic){
-    seed = static_cast<std::size_t>(std::stoi(v[7]));
-  }
+enum class GeneratorType{
+  N_M_N_E,
+  N_M_N_E_NNZ,
+  UNKOWN
+};
 
-  std::cout<<"Chain length: " <<chain_len<<'\n';
-  std::cout<<"Dimension range: [ "<<dim_lb<<" , "<<dim_ub<<" ]\n";
-  std::cout<<"Number of edges in the DAG range: [ "<<n_E_lb<<" , "<<n_E_ub<<" ]\n";
-  std::cout<<"Is deterministic: "<<std::boolalpha<<is_deterministic<<'\n';
-  
-  std::cout<<"Using constructor n_m_n_E_Generator(chain_len, dim_lb, dim_ub, n_E_lb, n_E_ub, is_deterministic, seed).\n";
-  n_m_n_E_Generator gen{chain_len, dim_lb, dim_ub, n_E_lb, n_E_ub, is_deterministic, seed};
+GeneratorType parse_generator(const std::string& name){
+  if (name == "n_m_n_E") return GeneratorType::N_M_N_E;
+  if (name == "n_m_n_E_nnz") return GeneratorType::N_M_N_E_NNZ;
+  return GeneratorType::UNKOWN;
+}
 
-  std::cout<<"Building problem.\n";
-  gen.build_problem();
+int main(){
 
-  std::cout<<"Printing problem.\n";
-  gen.print();
+  std::string input;
+  std::cout << "Enter generator type {n_m_n_E , n_m_n_E_nnz} \n";
+  std::cin >> input;
 
-  if(is_deterministic){
-    std::cout<<"Checking deterministic attribute of the generator.\n";
-    std::cout<<"Building problem.\n";
-    gen.build_problem();
+  GeneratorType type = parse_generator(input);
 
-    std::cout<<"Printing problem.\n";
-    gen.print();
-  }
+  switch (type){
+    case GeneratorType::N_M_N_E: {
+      std::size_t chain_len, dim_lb, dim_ub, n_E_lb, n_E_ub, seed;
+      bool is_deterministic, answer;
+      std::cout<< "Enter the arguments for the constructor.\n";
+      std::cout<< "chain_len dim_lb dim_ub n_E_lb n_E_ub is_deterministic{0,1} seed\n";
+      std::cin >> chain_len >> dim_lb >> dim_ub >> n_E_lb;
+      std::cin >> n_E_ub >> is_deterministic >> seed;
 
-  std::cout<<"Printing problem size: "<<gen.get_problem_size()<<'\n';
+      n_m_n_E_Generator gen{chain_len, dim_lb, dim_ub, n_E_lb, n_E_ub, is_deterministic, seed};
+      std::cout<< "Do you want to test build_problem? [0/1]\n";
+      std::cin >> answer;
+      if(answer){
+        gen.build_problem();
+        gen.print();
+      }
+      else{break;}
+      
+      std::cout<< "Do you want to test get_problem? [0/1]\n";
+      std::cin >> answer;
+      if(answer){
+        auto problem = gen.get_problem();
+        for(std::size_t i=0; i<gen.get_problem_size(); i++){
+          std::cout<< "F'_"<<i<<" [ ";
+          for(std::size_t j=0; j<problem[0].size(); j++){
+            std::cout<<problem[i][j]<<' ';
+          }
+          std::cout<<"]\n";
+        }
+      }
 
-  std::cout<<"Getting problem: get_problem()\n";
-  std::vector<std::vector<std::size_t>> problem = gen.get_problem();
-
-  for(std::size_t i=0; i<gen.get_problem_size(); i++){
-    std::cout<< "F'_"<<i<<" [ ";
-    for(std::size_t j=0; j<problem[0].size(); j++){
-      std::cout<<problem[i][j]<<' ';
+      std::cout<< "Do you want to check if the generator is deterministic? [0/1]\n";
+      std::cin >> answer;
+      if(answer){
+        std::cout<< "Calling build problem for the second time.\n";
+        gen.build_problem();
+        gen.print();
+      }
+      break;
     }
-    std::cout<<"]\n";
+    
+    case GeneratorType::N_M_N_E_NNZ: {
+      std::size_t chain_len, dim_lb, dim_ub, n_E_lb, n_E_ub, seed;
+      double den_lb, den_ub;
+      bool is_deterministic, answer;
+      std::cout<< "Enter the arguments for the constructor.\n";
+      std::cout<< "chain_len dim_lb dim_ub n_E_lb n_E_ub den_lb den_ub";
+      std::cout<< " is_deterministic{0,1} seed\n";
+      std::cin >> chain_len >> dim_lb >> dim_ub >> n_E_lb;
+      std::cin >> n_E_ub >> den_lb >> den_ub >>is_deterministic >> seed;
+
+      std::cout<< "Calling the constructor.";
+      n_m_n_E_nnz_Generator gen{chain_len, dim_lb, dim_ub, n_E_lb, n_E_ub, 
+          den_lb, den_ub, is_deterministic, seed};
+
+      std::cout<< "Do you want to test build_problem? [0/1]\n";
+      std::cin >> answer;
+      if(answer){
+        gen.build_problem();
+        gen.print();
+        break;
+      }
+      else{break;}
+      }
+
+    case GeneratorType::UNKOWN: {
+      std::cout<<"Invalid generator type.\n";
+      break;
+    }
   }
-
-  /* std::size_t i = 0; */
-  /* assert(i < chain_len); */
-
-  /* std::vector<size_t> F_i_desc{gen.read_Fi_description(i)}; */
-  /* std::cout<<"jac_chain_info["<< i <<"] = [ "; */
-
-  /* for(size_t data: F_i_desc){ */
-  /*   std::cout<< data<<' '; */
-  /* } */
-  /* std::cout<<"]\n"; */
   return 0;
 }
